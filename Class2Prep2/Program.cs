c4bf73aa5269debacd5e3629efb1438af9751c8d
Daniel <danielbrogers594@gmail.com>
2018-02-08 10:51:59 -0500

    first commit

---
+++ Class2Prep2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Class2Prep2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> stringList = new List<string>
+            {
+                "two",
+                "four",
+                "six",
+                "eight",
+                "ten",
+                "twelve",
+                "fourteen",
+                "sixteen",
+                "eighteen",
+                "twenty"
+
+
+            };
+            string[] numbers = stringList.ToArray();
+            foreach (string number in numbers)
+            {
+                string length = number.Length.ToString();
+                if (length == "5")
+                {
+                    Console.WriteLine(number);
+                    Console.ReadLine();
+                }
+            }
+        }
+        
+    }
+}
